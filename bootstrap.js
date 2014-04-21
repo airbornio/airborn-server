e3d69359878673219852faf9286fcc7bb3036bcb
Daniel Huigens <d.huigens@gmail.com>
2014-04-21 16:06:41 +0200

    Save user

--- bootstrap.js
+++ bootstrap.js
@@ -10,31 +10,7 @@ function GET(url, callback, authkey) {
 	req.send(null);
 }
 
-// Language strings
-GET('lang.json', function(response) {
-	var strings = JSON.parse(response);
-	document.getElementById('username').placeholder = strings.username;
-	document.getElementById('password').placeholder = strings.password;
-	document.getElementById('login').textContent = strings.login;
-});
-
-// Page content
-var iframe = document.createElement('iframe');
-if('sandbox' in iframe) {
-	iframe.sandbox = true;
-	iframe.src = 'content.html';
-	iframe.id = 'content';
-	document.documentElement.appendChild(iframe);
-}
-
-// Login handler
-document.getElementById('login').addEventListener('click', login);
-document.getElementById('password').addEventListener('keypress', function(evt) {
-	if(evt.keyCode === 13) login();
-});
-function login() {
-	var username = document.getElementById('username').value;
-	var password = document.getElementById('password').value;
+function login(username, password) {
 	GET('user/' + username + '/salt', function(salt) {
 		var key = sjcl.misc.pbkdf2(password, sjcl.codec.hex.toBits(salt), 1000);
 		var hmac = new sjcl.misc.hmac(key.slice(128/32));
@@ -42,4 +18,44 @@ function login() {
 			eval(sjcl.decrypt(password, response));
 		}, sjcl.codec.hex.fromBits(key.slice(0, 128/32)).toUpperCase());
 	});
+}
+
+var username = sessionStorage.username || localStorage.username;
+var password = sessionStorage.password || localStorage.password;
+if(username && password) {
+	
+	// Login with previously saved credentials
+	login(username, password);
+	
+} else {
+	
+	// Language strings
+	GET('lang.json', function(response) {
+		var strings = JSON.parse(response);
+		document.getElementById('username').placeholder = strings.username;
+		document.getElementById('password').placeholder = strings.password;
+		document.getElementById('login').value = strings.login;
+		document.getElementById('label').textContent = strings.save;
+	});
+
+	// Page content
+	var iframe = document.createElement('iframe');
+	if('sandbox' in iframe) {
+		iframe.sandbox = true;
+		iframe.src = 'content.html';
+		iframe.id = 'content';
+		document.documentElement.appendChild(iframe);
+	}
+	
+	// Login handler
+	document.getElementById('container').addEventListener('submit', function(evt) {
+		evt.preventDefault();
+		var username = document.getElementById('username').value;
+		var password = document.getElementById('password').value;
+		login(username, password);
+		var storage = document.getElementById('save').checked ? localStorage : sessionStorage;
+		storage.username = username;
+		storage.password = password;
+	}, false);
+	
 }
