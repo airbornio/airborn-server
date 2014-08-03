4fef80cb248d1d5bb032bfadfe2756585d813689
Daniel Huigens <d.huigens@gmail.com>
2014-08-03 23:27:12 +0200

    Remove unused PUT /object/:id endpoint

--- server.js
+++ server.js
@@ -106,48 +106,9 @@ app.get(/^\/sign_s3_(put|copy_(.+))$/, function(req, res) {
 	res.end();
 });
 
-app.put('/object/:id', function(req, res) {
-	if(!userLoggedIn(req)) {
-		res.send(403);
-		return;
-	}
-	var path = objPath(req);
-	var dest = fs.createWriteStream(path, {
-		flags: 'w'
-	});
-	dest.on('error', function(err) {
-		console.error(err.stack);
-		res.send(500);
-	});
-	req.pipe(dest);
-	req.on('end', function() {
-		console.log(process.env.DATABASE_URL);
-		pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-			if(err) {
-				console.error(err);
-				res.send(500);
-				return;
-			}
-			client.query('INSERT INTO objects (ID, userID, metadata) VALUES ($1, $2, $3)', [req.route.params.id, userID, req.get('X-Object-Metadata')], function() {
-				done();
-				if(err) {
-					console.error(err);
-					res.send(500);
-					return;
-				}
-				res.send(200);
-			});
-		});
-	});
-});
-
 var server = app.listen(process.env.PORT || 8080, function() {
 	console.log('Listening on port %d', server.address().port);
 });
-
-function objPath(req) {
-	return path.join(__dirname, 'object', req.route.params.id);
-}
 
 function login(req, res, authkey, cont) {
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
