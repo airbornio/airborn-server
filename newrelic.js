4bac0e25c1524da9a2b32c3ebd074c6c038b2815
Daniel Huigens <d.huigens@gmail.com>
2014-08-05 22:05:19 +0200

    Update names

--- newrelic.js
+++ newrelic.js
@@ -5,10 +5,6 @@
  * description of configuration variables and their potential values.
  */
 exports.config = {
-  /**
-   * Array of application names.
-   */
-  app_name : ['Airborn'],
   logging : {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
