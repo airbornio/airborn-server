1a0cbc392c55d4eee115c8b1a796b384b05db2d5
Daniel Huigens <d.huigens@gmail.com>
2014-08-05 15:10:29 +0200

    Remember files_key

--- bootstrap.js
+++ bootstrap.js
@@ -10,28 +10,39 @@ function GET(url, callback, authkey) {
 	req.send(null);
 }
 
-function login(username, password) {
+function login(username, password, files_key) {
 	GET('user/' + username + '/salt', function(salt) {
 		var key = sjcl.misc.pbkdf2(password, sjcl.codec.hex.toBits(salt), 1000);
 		var private_key = key.slice(128/32); // Second half
 		var shared_key = key.slice(0, 128/32); // First half
 		var private_hmac = window.private_hmac = new sjcl.misc.hmac(private_key);
 		var shared_hmac = window.shared_hmac = new sjcl.misc.hmac(shared_key);
-		GET('object/' + sjcl.codec.hex.fromBits(shared_hmac.mac(username).slice(0, 2)) + '/' + sjcl.codec.hex.fromBits(private_hmac.mac('/key')), function(response) {
-			var files_key = window.files_key = sjcl.codec.hex.toBits(sjcl.decrypt(password, response));
+		var authkey = sjcl.codec.hex.fromBits(shared_key).toUpperCase();
+		if(files_key) {
+			cont(authkey);
+		} else {
+			GET('object/' + sjcl.codec.hex.fromBits(shared_hmac.mac(username).slice(0, 2)) + '/' + sjcl.codec.hex.fromBits(private_hmac.mac('/key')), function(response) {
+				files_key = window.files_key = sjcl.codec.hex.toBits(sjcl.decrypt(password, response));
+				window.storage.files_key = JSON.stringify(files_key);
+				cont();
+			}, authkey);
+		}
+		function cont(authkey) {
 			GET('object/' + sjcl.codec.hex.fromBits(shared_hmac.mac(username).slice(0, 2)) + '/' + sjcl.codec.hex.fromBits(private_hmac.mac('/Core/init.js')), function(response) {
 				eval(sjcl.decrypt(files_key, response));
-			});
-		}, sjcl.codec.hex.fromBits(shared_key).toUpperCase());
+			}, authkey);
+		}
 	});
 }
 
 var username = sessionStorage.username || localStorage.username;
 var password = sessionStorage.password || localStorage.password;
-if(username && password) {
+var files_key = sessionStorage.files_key || localStorage.files_key;
+if(files_key) files_key = JSON.parse(files_key);
+if(username && password && files_key) {
 	
 	// Login with previously saved credentials
-	login(username, password);
+	login(username, password, files_key);
 	
 } else {
 	
@@ -59,7 +70,7 @@ if(username && password) {
 		var username = document.getElementById('username').value;
 		var password = document.getElementById('password').value;
 		login(username, password);
-		var storage = document.getElementById('save').checked ? localStorage : sessionStorage;
+		var storage = window.storage = document.getElementById('save').checked ? localStorage : sessionStorage;
 		storage.username = username;
 		storage.password = password;
 	}, false);
