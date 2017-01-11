9b28e7d059bb8115b5fa141234490b258dda141d
Daniel Huigens <d.huigens@gmail.com>
2017-01-11 03:15:08 +0100

    Refactor login.js to use Promises
    This also fixes the edge case that when the request to /key succeeds but
    the request to /hmac fails, it will fail silently.

--- login.js
+++ login.js
@@ -32,74 +32,75 @@ function login(creds, firstfile, requestmorecreds, success, error) {
 	var key = creds.key;
 	var files_key = creds.files_key;
 	var hmac_bits = creds.hmac_bits;
+	var authkey;
+	var storage = {};
 	if(username && (key || password)) {
-		GET('user/' + username + '/salt', function(salt) {
-			var done = 0;
-			var storage = {};
+		new Promise(function(resolve, reject) {
+			GET('user/' + username + '/salt', function(salt) {
+				creds.salt = salt = sjcl.codec.hex.toBits(salt);
+				resolve(salt);
+			}, reject);
+		}).then(function(salt) {
 			storage.username = window.username = username;
 			if(!key) {
-				key = sjcl.misc.pbkdf2(password, sjcl.codec.hex.toBits(salt), 1000);
+				key = sjcl.misc.pbkdf2(password, salt, 1000);
 				storage.key = key;
 			}
-			var private_key = window.private_key = key.slice(128/32); // Second half
+			window.private_key = key.slice(128/32); // Second half
 			var shared_key = key.slice(0, 128/32); // First half
-			var private_hmac = window.private_hmac = new sjcl.misc.hmac(private_key);
-			var files_hmac;
-			var authkey = sjcl.codec.hex.fromBits(shared_key).toUpperCase();
-			if(files_key && hmac_bits && firstfile) {
-				window.files_key = files_key;
-				files_hmac = window.files_hmac = new sjcl.misc.hmac(hmac_bits);
-				cont(authkey);
-			} else {
-				GET('object/' + sjcl.codec.hex.fromBits(private_hmac.mac('/key')), function(response) {
-					try {
-						files_key = decryptAndMaybeUngzip(private_key, response);
-					} catch(e) {
-						files_key = decryptAndMaybeUngzip(password, response);
-					}
-					storage.files_key = window.files_key = files_key = sjcl.codec.hex.toBits(files_key);
-					if(++done === 2) cont();
-				}, err, authkey);
-				GET('object/' + sjcl.codec.hex.fromBits(private_hmac.mac('/hmac')), function(response) {
-					try {
-						hmac_bits = decryptAndMaybeUngzip(private_key, response);
-					} catch(e) {
-						hmac_bits = decryptAndMaybeUngzip(password, response);
-					}
-					storage.hmac_bits = hmac_bits = sjcl.codec.hex.toBits(hmac_bits);
-					files_hmac = window.files_hmac = new sjcl.misc.hmac(hmac_bits);
-					if(++done === 2) cont();
-				}, undefined, authkey);
+			window.private_hmac = new sjcl.misc.hmac(window.private_key);
+			authkey = sjcl.codec.hex.fromBits(shared_key).toUpperCase();
+			
+			return Promise.all([
+				firstfile && files_key || new Promise(function(resolve, reject) {
+					GET('object/' + sjcl.codec.hex.fromBits(window.private_hmac.mac('/key')), function(response) {
+						try {
+							files_key = decryptAndMaybeUngzip(window.private_key, response);
+						} catch(e) {
+							files_key = decryptAndMaybeUngzip(password, response);
+						}
+						storage.files_key = files_key = sjcl.codec.hex.toBits(files_key);
+						resolve();
+					}, reject, authkey);
+				}),
+				hmac_bits || new Promise(function(resolve, reject) {
+					GET('object/' + sjcl.codec.hex.fromBits(window.private_hmac.mac('/hmac')), function(response) {
+						try {
+							hmac_bits = decryptAndMaybeUngzip(window.private_key, response);
+						} catch(e) {
+							hmac_bits = decryptAndMaybeUngzip(password, response);
+						}
+						storage.hmac_bits = hmac_bits = sjcl.codec.hex.toBits(hmac_bits);
+						resolve();
+					}, reject, authkey);
+				})
+			]);
+		}).then(function() {
+			window.files_key = files_key;
+			window.files_hmac = new sjcl.misc.hmac(hmac_bits);
+			
+			if(firstfile) {
+				return new Promise(function(resolve, reject) {
+					GET('object/' + sjcl.codec.hex.fromBits(window.files_hmac.mac(firstfile)), function(response) {
+						resolve(decryptAndMaybeUngzip(files_key, response))
+					}, reject, authkey);
+				});
 			}
-			function cont(authkey) {
-				function cont2() {
-					var account_info = window.account_info = JSON.parse(decodeURIComponent(document.cookie.split('=')[1]).match(/\{.*\}/)[0]);
-					var S3Prefix = window.S3Prefix = account_info.S3Prefix;
-					var account_version = window.account_version = account_info.account_version;
-					if(account_version === 1) {
-						storage.password = window.password = password;
-					}
-				}
-				if(firstfile) {
-					GET('object/' + sjcl.codec.hex.fromBits(files_hmac.mac(firstfile)), function(response) {
-						cont2();
-						eval(decryptAndMaybeUngzip(files_key, response));
-						success(storage);
-					}, err, authkey);
-				} else {
-					cont2();
-					success(storage);
-				}
+		}).then(function(firstfilecontents) {
+			window.account_info = JSON.parse(decodeURIComponent(document.cookie.split('=')[1]).match(/\{.*\}/)[0]);
+			window.S3Prefix = window.account_info.S3Prefix;
+			window.account_version = window.account_info.account_version;
+			if(window.account_version === 1) {
+				storage.password = window.password = password;
 			}
-			function err(req) {
-				if(req.status === 401) {
-					error({status: req.status, statusText: req.statusText, id: 'wrongpass'});
-				} else {
-					error({status: req.status, statusText: req.statusText, id: 'error'});
-				}
+			if(firstfile) {
+				eval(firstfilecontents);
 			}
-		}, function(req) {
-			if(req.status === 404) {
+			success(storage);
+		}).catch(function(req) {
+			if(req.status === 401) {
+				error({status: req.status, statusText: req.statusText, id: 'wrongpass'});
+			} else if(req.status === 404) {
 				error({status: req.status, statusText: req.statusText, id: 'wronguser'});
 			} else {
 				error({status: req.status, statusText: req.statusText, id: 'error'});
