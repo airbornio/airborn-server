5f18179e39843f2368b881b8ea9f19f1e95b8694
Daniel Huigens <d.huigens@gmail.com>
2017-08-15 00:41:15 +0200

    Allow browser to cache resources for a short period

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
 	if(req.method === 'GET') {
-		res.set('Cache-control', 's-maxage=86400, max-age=0');
+		res.set('Cache-control', 's-maxage=86400, max-age=300');
 		res.set('X-GitHub-Commit', process.env.HEROKU_SLUG_COMMIT);
 	}
 	next();
